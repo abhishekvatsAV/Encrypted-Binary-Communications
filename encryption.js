a489e7cdd89399dbea4858034f42245851076d7b
Abhishek Vats <virus@luffy-book.local>
2024-01-17 13:33:54 +0530

    Working with single message

--- encryption.js
+++ encryption.js
@@ -1,25 +1,3 @@
-// import crypto from "node:crypto"
-
-// class Encryption {
-//   static encrypt(data, key) {
-//     const iv = crypto.randomBytes(12);
-//     const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
-//     let encryptedData = cipher.update(data, "utf8", "base64");
-//     encryptedData += cipher.final("base64");
-//     const tag = cipher.getAuthTag();
-//     return { iv, encryptedData, tag };
-//   }
-
-//   static decrypt(encryptedData, key, iv, tag) {
-//     const decipher = crypto.createDecipheriv("aes-256-gcm", key, iv);
-//     decipher.setAuthTag(tag);
-//     const decryptedData = decipher.update(encryptedData, "base64", "utf8") + decipher.final("utf8");
-//     return decryptedData;
-//   }
-// }
-
-// export default Encryption;
-
 import CryptoJS from "crypto-js";
 
 class Encryption {
@@ -30,7 +8,7 @@ class Encryption {
   }
 
   static decrypt(encryptedData, key, iv) {
-    console.log("here????")
+    console.log("here????");
     const decryptedData = CryptoJS.AES.decrypt(encryptedData, key, { iv });
     return decryptedData.toString(CryptoJS.enc.Utf8);
   }
